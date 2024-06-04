3b5e072bab430bd03510166127dc274c75cc7bcd
Theo Reichert <theo.reichert7@gmail.com>
2024-06-04 04:04:41 +0200

    Improve /mongodb/run-sh.sh usage

--- mongodb/run.sh
+++ mongodb/run.sh
@@ -7,7 +7,22 @@ kubectl apply -f ./deployment.yaml
 kubectl apply -f ./mongo-express/service.yaml
 kubectl apply -f ./mongo-express/deployment.yaml
 
-sleep 5
+echo 'Close the following plane after all services and deployments are ready.'
+case $SHELL in
+  '/bin/bash')
+    read -n1 -r -s
+    ;;
+  '/bin/zsh')
+    read -k1 -rs
+    ;;
+  *)
+    sleep 8
+    ;;
+esac
 
+watch --color -d kubectl get all
+
+echo 'the standard credentials for the login in the webinterface\n
+     according to https://github.com/mongo-express/mongo-express\n
+     admin pass'
 minikube service mongodb-express-s
-watch kubectl get all
