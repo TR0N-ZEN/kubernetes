cd3ae74831cb376d4e560d21f0d458dccad8b1dc
Theo Reichert <theo.reichert7@gmail.com>
2024-06-13 14:12:26 +0200

    Working ingress

--- mongodb/run.sh
+++ mongodb/run.sh
@@ -14,25 +14,28 @@ kubectl apply -f ./mongo-express/deployment.yaml
 #   the minikube automatic way
 minikube addons enable ingress
 
-echo 'Close the following plane after all services and deployments are ready.'
-case $SHELL in
-  '/bin/bash')
-    read -n1 -r -s
-    ;;
-  '/bin/zsh')
-    read -k1 -rs
-    ;;
-  *)
-    sleep 8
-    ;;
-esac
+watch --color -d '\
+  echo "Close the following plane after all services and deployments are ready."; \
+  kubectl get all; echo ""; \
+  kubectl get ingressClass;'
+
 
 kubectl apply -f ./mongo-express/ingress.yaml
+watch --color -d '\
+  echo "Close the plane after all admission pods finished and the controller is running."; \
+  kubectl get pods --namespace ingress-nginx'
 
+echo "if the following command returns: 'Unauthorized', everything should be working"
+curl --resolve "a.b:80:`minikube ip`" http://a.b/
 
-
-watch --color -d 'kubectl get all; echo ""; kubectl get ingress; echo ""; kubectl get ingressClass'
-
-echo 'the standard credentials for the login in the webinterface\n
+echo "\n
+     add the line \n
+        `minikube ip` a.b\n
+     to the file /etc/hosts !!!\n
+     \n
+     execute `xdg-open a.b` which will open your browser and use\n
+     the standard credentials for the login in the webinterface\n
      according to https://github.com/mongo-express/mongo-express\n
-     admin pass'
+     admin pass"
+
+
