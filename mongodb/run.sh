4eb812c82191186f5870a4beba90316338299644
Theo Reichert <theo.reichert7@gmail.com>
2024-06-28 04:54:25 +0200

    Everything starts but I cant connect

--- mongodb/run.sh
+++ mongodb/run.sh
@@ -4,19 +4,11 @@ kubectl apply \
   -f ./service.yaml \
   -f ./deployment.yaml \
   -f ./mongo-express/deployment.yaml
-
-echo 'Close the following plane after all services and deployments are ready.'
-case $SHELL in
-  '/bin/bash')
-    read -n1 -r -s
-    ;;
-  '/bin/zsh')
-    read -k1 -rs
-    ;;
-  *)
-    sleep 8
-    ;;
-esac
+kubectl apply -f ./configmap.yaml -f ./secret.yaml
+kubectl apply -f ./service.yaml 
+kubectl apply -f ./deployment.yaml 
+kubectl apply -f ./mongo-express/service.yaml
+kubectl apply -f ./mongo-express/deployment.yaml
 
 watch --color -d 'kubectl get pods; kubectl get services;'
 
@@ -32,7 +24,16 @@ if [ $1 -eq 'nodePort.service' ]; then
   watch --color -d 'kubectl get service;'
 fi
 if [ $1 -eq 'gateway' ]; then
+  # don't know if this is necessary
   # kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.1.0/standard-install.yaml
+
+  # can probably deleted because of next paragraph
+  # kubectl apply -f https://raw.githubusercontent.com/projectcontour/contour/main/examples/gateway/00-crds.yaml
+  # kubectl apply -f https://projectcontour.io/quickstart/contour.yaml
+
+  # read https://projectcontour.io/docs/main/guides/gateway-api/
+  kubectl apply -f https://projectcontour.io/quickstart/contour-gateway-provisioner.yaml
+
   kubectl apply \
     -f ./mongo-express/service.yaml \
     -f ./mongo-express/gatewayClass.yaml \
