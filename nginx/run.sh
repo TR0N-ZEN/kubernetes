d2137afbb40c370de8b92c0e7577507de0cafeff
Theo Reichert <theo.reichert7@gmail.com>
2025-07-21 13:38:01 +0200

    Removes assumption using 'minikube'

--- nginx/run.sh
+++ nginx/run.sh
@@ -1,24 +1,4 @@
-minikube delete
-minikube start
-minikube addons enable ingress
-
-echo 'Close the following plane after all services and deployments are ready.'
-case $SHELL in
-  '/bin/bash')
-    read -n1 -r -s
-    ;;
-  '/bin/zsh')
-    read -k1 -rs
-    ;;
-  *)
-    sleep 8
-    ;;
-esac
-
-watch --color -d 'kubectl get pods -n ingress-nginx'
-
-
-for item in 'service' 'ingress' 'deployment'; do
+for item in 'deployment' 'service' 'ingress'; do
   kubectl apply -f $item.yaml
 done;
 
@@ -36,5 +16,3 @@ case $SHELL in
 esac
 
 watch --color -d 'kubectl get pods; kubectl get services; kubectl get ingress'
-
-curl `minikube ip`
