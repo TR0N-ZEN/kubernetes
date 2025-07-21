42d260be563fcbf5e5de914b0ad9d7c6990e3105
Theo Reichert <theo.reichert7@gmail.com>
2025-07-21 18:21:28 +0200

    Updates run.sh files

--- mongodb/run.sh
+++ mongodb/run.sh
@@ -1,6 +1,33 @@
-kubectl apply -f ./configmap.yaml -f ./secret.yaml
-kubectl apply -f ./service.yaml 
-kubectl apply -f ./deployment.yaml 
-kubectl apply -f ./mongo-express/service.yaml
-kubectl apply -f ./mongo-express/deployment.yaml
-kubectl apply -f ./mongo-express/ingress.yaml
+kubectl apply \
+  -f ./configmap.yaml \
+  -f ./secret.yaml \
+  -f ./service.yaml \
+  -f ./deployment.yaml \
+  -f ./mongo-express/deployment.yaml
+
+echo 'Close the following plane after all services and deployments are ready.'
+case $SHELL in
+  '/bin/bash')
+    read -n1 -r -s
+    ;;
+  '/bin/zsh')
+    read -k1 -rs
+    ;;
+  *)
+    sleep 8
+    ;;
+esac
+
+watch --color -d 'kubectl get pods; kubectl get services;'
+
+if [ $1 -eq 'ingress' ]; then
+  kubectl apply \
+    -f ./mongo-express/service.yaml \
+    -f ./mongo-express/ingress.yaml
+  watch --color -d 'kubectl get service; kubectl get ingress;'
+fi
+if [ $1 -eq 'nodePort.service' ]; then
+  kubectl apply \
+    -f ./mongo-express/nodePort.service.yaml
+  watch --color -d 'kubectl get service;'
+fi
--- nginx/run.sh
+++ nginx/run.sh
@@ -1,6 +1,6 @@
-for item in 'deployment' 'service' 'ingress'; do
-  kubectl apply -f $item.yaml
-done;
+kubectl apply \
+  -f ./service.yaml \
+  -f ./deployment.yaml
 
 echo 'Close the following plane after all services and deployments are ready.'
 case $SHELL in
@@ -15,4 +15,17 @@ case $SHELL in
     ;;
 esac
 
-watch --color -d 'kubectl get pods; kubectl get services; kubectl get ingress'
+watch --color -d 'kubectl get pods; kubectl get services;'
+
+
+if [ $1 -eq 'ingress' ]; then
+  kubectl apply \
+    -f ./service.yaml \
+    -f ./ingress.yaml
+  watch --color -d 'kubectl get service; kubectl get ingress;'
+fi
+if [ $1 -eq 'nodePort.service' ]; then
+  kubectl apply \
+    -f ./nodePort.service.yaml
+  watch --color -d 'kubectl get service;'
+fi
